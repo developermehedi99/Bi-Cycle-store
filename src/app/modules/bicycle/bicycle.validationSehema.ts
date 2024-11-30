ca64eded43dc1d64009867573be49dffe417e32b
Mehedi Hasan <mdmehedihasan20010@gmail.com>
2024-11-30 11:52:54 +0600

    bicycle done

--- src/app/modules/bicycle/bicycle.validationSehema.ts
+++ src/app/modules/bicycle/bicycle.validationSehema.ts
@@ -14,20 +14,3 @@ export const BicycleValidationZodSehema = z.object({
     .nonnegative('Quantity must be a non-negative integer.'),
   inStock: z.boolean(),
 });
-
-// const BicycleValidationZodSehema = z.object({
-//   name: z.string({ message: 'Not valid input' }),
-//   brand: z.string({ message: 'Not valid input' }),
-//   price: z.number().positive({ message: 'Price must be a positive number' }),
-//   category: z.enum(['Mountain', 'Road', 'Hybrid', 'BMX', 'Electric'], {
-//     message: 'Not valid input',
-//   }),
-//   description: z.string({ message: 'Not valid input' }), // description is required and must be a string
-//   quantity: z
-//     .number()
-//     .int({ message: 'Quantity must be an integer' })
-//     .min(1, { message: 'Quantity must be at least 1' }),
-//   inStock: z.boolean(),
-// });
-
-// export default BicycleValidationZodSehema;
